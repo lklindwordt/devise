738f2ccf8a829dd85bcd72f458e6b426148df1bd
Lars Klindwordt <info@cobarian.de>
2021-09-19 16:55:39 +0200

    Remove ActiveSupport::Dependencies.reference

--- test/rails_app/config/boot.rb
+++ test/rails_app/config/boot.rb
@@ -8,8 +8,8 @@ module Devise
   module Test
     # Detection for minor differences between Rails versions in tests.
     
-    def self.rails6?
-      Rails.version.start_with? '6'
+    def self.rails6_and_up?
+      Rails::VERSION::MAJOR >= 6
     end
 
     def self.rails52_and_up?
